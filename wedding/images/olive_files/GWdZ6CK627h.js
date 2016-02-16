e10871c20b24a84a940d93ea3e0d014013102d84
Ava <avayeh20099@gmail.com>
2016-02-16 19:47:34 +0000

    initial

---
+++ wedding/images/olive_files/GWdZ6CK627h.js
@@ -0,0 +1,5 @@
+/*!CK:1039756355!*//*1455236893,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["kCv+r"]); }
+
+__d('MercuryGiftSnippetRenderer',['fbt'],function a(b,c,d,e,f,g,h){'use strict';if(c.__markCompiled)c.__markCompiled();var i={renderText:function(j,k){return j?h._("You sent a valentine."):h._("{name} sent a valentine.",[h.param('name',k)]);}};f.exports=i;},null);
---
+++ wedding/images/olive_files/WAmlPPD1l9J.js
@@ -0,0 +1,5 @@
+/*!CK:452257492!*//*1455460080,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
+
+__d('legacy:PhotoSnowliftAds',['PhotoSnowliftAds'],function a(b,c,d,e){if(c.__markCompiled)c.__markCompiled();b.PhotoSnowliftAds=c('PhotoSnowliftAds');},3);
